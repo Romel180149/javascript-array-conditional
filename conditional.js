9be2c8372986239e7e9109720820fa5329daf901
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-30 11:24:05 +0600

    conditional code added

---
+++ conditional.js
@@ -0,0 +1,19 @@
+var iPhonePrice=79000;
+var myBudget=9500;
+if (iPhonePrice<myBudget){
+    // console.log('i phone kine photqtin marbo')
+};
+var chickenPrice=500;
+var myMoney=50;
+if(chickenPrice <= myMoney){
+    // console.log('murgir ran khamu haddi chabamu');
+}
+if(chickenPrice > myMoney){
+    // console.log('smashed potataoto and lentil soup');
+}
+if(chickenPrice < myMoney){
+    console.log('i will eat chicken');
+}
+else{
+    console.log('i will eat potato');
+}
