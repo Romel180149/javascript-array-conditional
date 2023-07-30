48ea156eedb1650e117b5b9af12e4e820edd032a
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-30 10:11:56 +0600

    push pop added

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 var numbers=[10,20,30,40,59];
 // get elements by index
-console.log(numbers[4])
+var elements=numbers[1];
+// set element by value
+numbers[1]=77;
+console.log(numbers);
+numbers[3]=30;
+console.log(elements);
+// find the index of an element
+var positionIndex=numbers.indexOf(59);
+console.log(positionIndex)
---
+++ pushPop.js
@@ -0,0 +1,14 @@
+var numbers=[10,20,30,40,50];
+numbers.push(90);
+numbers.pop(90);
+// console.log(numbers);
+var friends=['balam','kalam','salam'];
+friends.push('gelam');
+friends.push('pelam');
+
+// console.log(friends);
+// use pop to get last element
+console.log(numbers);
+var element =numbers.pop();
+// console.log(numbers);
+console.log(element);
