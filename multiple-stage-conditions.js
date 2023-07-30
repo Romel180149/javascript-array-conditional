6c3806d39c5fad179684b9e564d77fa73ac907fe
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-30 18:48:49 +0600

    nested con

---
+++ multiple-stage-conditions.js
@@ -0,0 +1,17 @@
+var money=40;
+var danishprice=45;
+var butterBread=35;
+var toastBiscuir=30;
+if(danishprice < money)
+{
+    console.log('dan dan danish khamu');
+}
+else if(butterBread < money){
+    console.log('butter boan khamu')
+}
+else if(toastBiscuir < money){
+    console.log('chubai toast biscuit khamu');
+}
+else{
+    console.log('shudu cha e khabo')
+}
---
+++ nested-condition.js
@@ -0,0 +1,18 @@
+var math=true;
+var geometry=true;
+var straightlint =false;
+
+if(math==true){
+    if(geometry==true){
+        if(straightlint==false){
+
+        }
+        else{
+            console.log('baka pothe cholbo na');
+        }
+
+    }
+    else {
+        console.log('pithagorus hoita parba na')
+    }
+}
