d7902688a965027b128d349c01df5fdb5f1c41aa
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-30 18:32:55 +0600

    multiple condition code added

---
+++ multiple-condition.js
@@ -0,0 +1,27 @@
+// var isGraduated=true;
+// var salary=25000;
+// var cars=0;
+var isGraduated=true;
+var salary=75000;
+var cars=1;
+if(isGraduated==true && salary>50000 || cars>=1){
+    console.log('eso prem kori');
+}
+// if(isGraduated==true || salary>50000 || cars>=1){
+//     console.log('eso prem kori');
+// }
+else {
+    console.log('tumar friend phn nb deu');
+}
+// if(isGraduated==true || salary>50000  ||cars >=1){
+//     console.log('eso prem kori');
+// }
+// else{
+//     console.log('tmr friend er phone nb deu');
+// }
+// if (isGraduated==true && salary>50000 && cars>=1){
+// console.log('eso biye kore feli');
+// }
+// else {
+// console.log('tor kopale biye nai');
+// }
