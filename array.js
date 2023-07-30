10b201b2ddb56c42f8c5d3ad8e43c2535d2e2b3a
Romel180149 <mosiurrahmanromel180149@gmail.com>
2023-07-30 09:45:29 +0600

    array code

---
+++ array.js
@@ -0,0 +1,5 @@
+var friendsAge=[11, 21 ,45, 17 ,14 ,105, 6];
+var picnicFee=[1000, 4000, 500];
+var nayikas=['mahi', 'opu bissas', 'sabnur', 'sabana'];
+var vowels=['a' ,'e','i','o','u'];
+console.log(friendsAge.length);
---
+++ index.js
@@ -0,0 +1,3 @@
+var numbers=[10,20,30,40,59];
+// get elements by index
+console.log(numbers[4])
